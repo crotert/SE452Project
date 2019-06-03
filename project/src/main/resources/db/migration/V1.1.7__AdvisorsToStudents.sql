c1967042d886e4bcbf95ffcb904adda71eef6d66
oakeswf14 <49377530+oakeswf14@users.noreply.github.com>
2019-06-02 22:28:02 -0500

    Update and rename V1.1.7_AdvisorsToStudents.sql to V1.1.7__AdvisorsToStudents.sql

--- project/src/main/resources/db/migration/V1.1.7__AdvisorsToStudents.sql
+++ project/src/main/resources/db/migration/V1.1.7__AdvisorsToStudents.sql
@@ -1,6 +1,6 @@
 Create table AdvisorToStudents(
     EmployeeID int not null Foreign Key references PROFESSORS(employee_id),
-    StudentID int not null Primary Key Foreign Key references STUDENTS(student_id));
+    StudentID int not null Foreign Key references STUDENTS(student_id) Primary Key);
     
 insert into AdvisorToStudents (EmployeeID,StudentID)
 Values(890890,321321);
