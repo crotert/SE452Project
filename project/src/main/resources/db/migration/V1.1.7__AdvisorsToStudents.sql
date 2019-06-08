129a13ddd23b4def26dc4f10e6db9751b0e85c10
oakeswf14 <49377530+oakeswf14@users.noreply.github.com>
2019-06-08 02:09:03 -0500

    Update V1.1.7__AdvisorsToStudents.sql

--- project/src/main/resources/db/migration/V1.1.7__AdvisorsToStudents.sql
+++ project/src/main/resources/db/migration/V1.1.7__AdvisorsToStudents.sql
@@ -1,6 +1,7 @@
 Create table AdvisorToStudents(
     employee_id int not null,
-    student_id int not null primary key);
+    student_id int not null primary key,
+   foreign key (student_id) references STUDENTS(student_id));
     
 insert into AdvisorToStudents (employee_id,student_id)
 Values(890890,321321);
