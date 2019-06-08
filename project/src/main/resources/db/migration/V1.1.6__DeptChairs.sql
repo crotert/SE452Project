f2bfdcee135f94cc1dbd6eb0d2291ee30211e385
oakeswf14 <49377530+oakeswf14@users.noreply.github.com>
2019-06-08 02:08:26 -0500

    Update V1.1.6__DeptChairs.sql

--- project/src/main/resources/db/migration/V1.1.6__DeptChairs.sql
+++ project/src/main/resources/db/migration/V1.1.6__DeptChairs.sql
@@ -1,6 +1,7 @@
 create table DeptChairs(
   EmployeeID identity not null primary key, 
-  Dept Varchar(50) not null);
+  Dept Varchar(50) not null,
+  foreign key (EmployeeID) references PROFESSORS(employee_id));
   
 Insert into DeptChairs(EmployeeID, Dept)
 Values(890890,'Computer Science');
