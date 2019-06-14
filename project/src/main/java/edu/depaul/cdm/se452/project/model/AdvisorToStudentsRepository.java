85a4a348f8cabb19abf6eec0a774d90ccbbe5fcc
oakeswf14 <49377530+oakeswf14@users.noreply.github.com>
2019-06-13 19:48:28 -0500

    Update AdvisorToStudentsRepository.java

--- project/src/main/java/edu/depaul/cdm/se452/project/model/AdvisorToStudentsRepository.java
+++ project/src/main/java/edu/depaul/cdm/se452/project/model/AdvisorToStudentsRepository.java
@@ -3,8 +3,19 @@ package edu.depaul.cdm.se452.project.model;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.mongodb.repository.MongoRepository;
 
 //repository class to help perform CRUD operations on the courses table
-public interface AdvisorToStudentsRepository extends CrudRepository<AdvisorToStudents, Long> {
+public interface AdvisorToStudentsRepository extends CrudRepository<Advisors_To_Students, Long> {
+	
+	List<Advisors_To_Students> findByEmployeeid(int num);
+	
+	List<Advisors_To_Students> findByStudentid(int num);
+	@Override
+	//returns all advisors available
+	List<Advisors_To_Students> findAll();
+	
+	
+	
 
 }
